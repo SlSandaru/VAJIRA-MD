d3cfba7536607b06aa6f5ebb44221b760d8097f8
SlSandaru <120253022+SlSandaru@users.noreply.github.com>
2024-07-31 18:47:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
-const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
+const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ5';
 global.owner = process.env.OWNER_NUMBER  || '94783681488' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
